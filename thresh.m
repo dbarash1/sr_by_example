cfdda124c185adbd37ff60f0666c507f54ed487e
Danny Barash <danny.barash@intel.com>
2015-08-25 12:07:13 +0300

    Revert "Initial commit."
    This reverts commit c57cd394f4efcffbed865596900d2dce4d544078.

--- thresh.m
+++
@@ -1,7 +0,0 @@
-function [ t ] = thresh( im, im_translated, patch_center_x,patch_center_y)
-    global STEP;
-    p1 = coord2patch( im,patch_center_x,patch_center_y,STEP );
-    p2 = coord2patch( im_translated,patch_center_x,patch_center_y,STEP );
-    t = distance(p1,p2);
-end
-    
